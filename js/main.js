d7c17197be6efe736752231cef8c5de51e767531
gbadamosi11 <gbadamosimubarak60@gmail.com>
2021-06-21 08:55:35 -0700

    htmls

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-const more = document.querySelector('.continue');
-const less = document.querySelector('.less');
-const hide = document.querySelector('.hide');
-more.classList.add('show');
-more.addEventListener('click', function(){
-     hide.classList.toggle('show-hide');
-     more.classList.toggle('show');
-     less.classList.add('show')
-});
-less.addEventListener('click', function(){
-     hide.classList.toggle('show-hide');
-     less.classList.toggle('show');
-     more.classList.add('show');
-});
+// const more = document.querySelector('.continue');
+// const less = document.querySelector('.less');
+// const hide = document.querySelector('.hide');
+// more.classList.add('show');
+// more.addEventListener('click', function(){
+//      hide.classList.toggle('show');
+//      more.classList.toggle('show');
+//      less.classList.add('show')
+// });
+// less.addEventListener('click', function(){
+//      hide.classList.toggle('show-hide');
+//      less.classList.toggle('show');
+//      more.classList.add('show');
+// });
 
